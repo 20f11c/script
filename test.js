06c063722d59ecac8e919a2cfe252196e82982c8
u013276346 <u013276346@noreply.gitcode.com>
2024-05-14 09:17:59 +0800

    Automated commit at 2024-05-14 09:17:59\n\n[Updated] test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
+/**
+ * @description 测试代理
+ */
 const Request = require('./lib/Axios');
-
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
-
 const $ = new Request(userAgent);
-
 (async () => {
   const headers = {
     Accept: 'application/json, text/plain, */*',
